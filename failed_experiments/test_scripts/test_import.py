8bc4733fc440189180d3313864b105361b0bb176
Sasank Garimella <gssasank@gmail.com>
2025-11-24 00:51:54 -0500

    fix: almost close to the final submission

--- failed_experiments/test_scripts/test_import.py
+++
@@ -1,7 +0,0 @@
-print("Start import test", flush=True)
-import torch
-print("Imported torch", flush=True)
-import clip
-print("Imported clip", flush=True)
-from datasets import load_dataset
-print("Imported datasets", flush=True)
